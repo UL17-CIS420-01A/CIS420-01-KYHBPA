1601cc5729de446b9eae5ed1b8b3a9de1e517ee0
Rakesh R Mahbubani <rrmahb01@cardmail.louisville.edu>
2017-09-06 15:20:38 -0400

    changed message in controller

--- KYHBPA.Web/Controllers/HomeController.cs
+++ KYHBPA.Web/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace KYHBPA.Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message="Your application description page.";
+            ViewBag.Message="Your done goofed Louis!";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message="Your contact page.";
+            ViewBag.Message="This might be your contact page. What do you think?";
 
             return View();
         }
