ba0fcbd0786372379b9f17701b146e0bf303d136
Louis S Ries <lsries01@cardmail.louisville.edu>
2017-09-06 12:53:52 -0400

    Added Entity models to KYHBPA.Data, Added UserRepository

---
+++ KYHBPA.Data/Infrastructure/Entities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KYHBPA.Data.Entity;
+
+namespace KYHBPA.Data.Infrastructure
+{
+    public class Entities : KYHBPAEntities
+    {
+        public Entities() : base()
+        {
+        }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+        
+        public static Entities Create()
+        {
+            return new Entities();
+        }
+    }
+}
---
+++ KYHBPA.Data/Repository/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KYHBPA.Data.Entity;
+
+namespace KYHBPA.Data.Repository
+{
+    public class UserRepository
+    {
+        public AspNetUser GetUser(Guid userId)
+        {
+            using (var context = new KYHBPAEntities())
+            {
+                var user = context.AspNetUsers.AsNoTracking().Where(o => o.Id == userId.ToString()).FirstOrDefault();
+                return user;
+            }
+        }
+        public AspNetUser GetUser(string userName)
+        {
+            using (var context = new KYHBPAEntities())
+            {
+                var user = context.AspNetUsers.AsNoTracking().Where(o => o.UserName == userName.ToString()).FirstOrDefault();
+                return user;
+            }
+        }
+
+        public List<AspNetUser> GetUsers()
+        {
+            using (var context = new KYHBPAEntities())
+            {
+                var users = context.AspNetUsers.AsNoTracking();
+                return users.ToList();
+            }
+        }
+        public bool IsInRole(string role, Guid userId)
+        {
+            return GetUsers().Any(u => u.AspNetRoles.Any(r => r.Name == role));
+        }
+    }
+}
