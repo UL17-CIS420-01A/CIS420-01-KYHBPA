9b24c9dace09b3e86b87aca45e3dd7e7e5b26f21
Rakesh R Mahbubani <rrmahb01@cardmail.louisville.edu>
2017-10-16 14:10:39 -0400

    changed 'feedbacks' to feedback added a blog model, view, and controller added blog to the navbar

---
+++ KYHBPA.Web/Models/BlogViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KYHBPA.Web.Models
+{
+    public class BlogViewModel
+    {
+        public int id { get; set; }
+        public string message { get; set; }
+    }
+}
