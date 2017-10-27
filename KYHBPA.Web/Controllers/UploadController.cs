b1936f1f265d7377f7347361c65c020df902c557
Rakesh R Mahbubani <rrmahb01@cardmail.louisville.edu>
2017-10-27 14:39:03 -0400

    controller for uploading files

---
+++ KYHBPA.Web/Controllers/UploadController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KYHBPA.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (File.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "Success";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Failure";
+                return View();
+            }
+        }
+
+    }
+}
