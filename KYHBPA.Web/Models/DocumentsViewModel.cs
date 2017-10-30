82fe04a1375295afb8dc4e2c6c235c3efed9d19e
Rakesh R Mahbubani <rrmahb01@cardmail.louisville.edu>
2017-10-30 15:17:18 -0400

    Documents View Model

---
+++ KYHBPA.Web/Models/DocumentsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Web.Mvc;
+using KYHBPA.ActionResults;
+
+namespace KYHBPA.Models
+{
+    public class DocumentsViewModel
+    {        
+        public int ID { get; set; }
+        public string Extension { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "File Name")]
+        public string FileName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+    }
+}
