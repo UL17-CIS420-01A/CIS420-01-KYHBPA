1550b1c8a8cda2330863998b70a674081c449d58
Rakesh R Mahbubani <rrmahb01@cardmail.louisville.edu>
2017-11-03 16:04:11 -0400

    document upload completed model based on photos

--- KYHBPA.Web/Controllers/DocumentsController.cs
+++ KYHBPA.Web/Controllers/DocumentsController.cs
@@ -8,30 +8,6 @@ namespace KYHBPA.Controllers
 {
     public class DocumentsController : Controller
     {
-        // GET: Documents
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult Upload()
-        {
-            return View();
-        }
-
-        public ActionResult Download()
-        {
-            return View();
-        }
-
-        public ActionResult Edit()
-        {
-            return View();
-        }
-
-        public ActionResult Delete()
-        {
-            return View();
-        }
     }
-}
+
+    
--- KYHBPA.Web/Models/DocumentsViewModel.cs
+++ KYHBPA.Web/Models/DocumentsViewModel.cs
@@ -8,18 +8,87 @@ using KYHBPA.ActionResults;
 
 namespace KYHBPA.Models
 {
-    public class DocumentsViewModel
-    {        
-        public int ID { get; set; }
+    public class DocumentsListViewModel
+    {
+        public ApplicationUser CurrentUser { get; set; }
+
+        public IEnumerable<Guid> Ids { get; set; }
+    }
+
+    public class DocumentUploadViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Document Name")]
+        public string DocumentName { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Description")]
+        public string Descritpion { get; set; }
+
+        [Display(Name = "Show in list?")]
+        [DefaultValue(true)]
+        public bool IsInList { get; set; }
+
+        [Required]
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase DocumentData { get; set; }
+
+        [Display(Name = "Include in Collection")]
+        public string DocumentCollectionKey { get; set; }
+
+        public IEnumerable<SelectListItem> DocumentKeys { get; set; }
+    }
+
+    public class DocumentDetailedViewModel
+    {
+        [Required]
+        public Guid Id { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
-        [Display(Name = "Title")]
-        public string Title { get; set; }
+        [Display(Name = "Document Name")]
+        public string DocumentName { get; set; }
 
         [Required]
         [DataType(DataType.MultilineText)]
         [Display(Name = "Description")]
         public string Description { get; set; }
+
+        public Member UploadedBy { get; set; }
+        public DateTime Uploaded { get; set; }
+        public Member LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+        public bool IsDeleted { get; set; }
+        public Member DeletedBy { get; set; }
+        public DateTime? Deleted { get; set; }
+
+        [Display(Name = "Show in list?")]
+        public bool IsInList { get; set; }
+
+        [Display(Name = "Collection:")]
+        public string DocumentCollectionKey { get; set; }
+    }
+
+    public class DocumentEditViewModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required] 
+        [DataType(DataType.Text)]
+        [Display(Name = "Document Name")]
+        public string DocumentName { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Show in list?")]
+        public bool IsInList { get; set; }
+
+        public IEnumerable<SelectListItem> DocumentKeys { get; set; }
     }
 }
